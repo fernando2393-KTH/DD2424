8feee1e11a9f789385fcc76ae34b20cb7863151e
fernando2393 <garciasanzfernando2393@gmail.com>
2020-03-25 16:28:50 +0100

    Bonus point 1 finished. Bonus point 2 implemented (check proper performance).

--- Assignment 1/Assignment1.py
+++ Assignment 1/Assignment1.py
@@ -7,6 +7,11 @@ LENGTH = 1024  # Number of pixels of the image
 SIZE = 32  # Pixel dimension of the image
 D_BATCH = ['data_batch_1', 'data_batch_2', 'data_batch_3', 'data_batch_4', 'data_batch_5']
 T_BATCH = 'test_batch'
+FULL_TRAIN = False
+SIZE_VAL = 1000  # Number of elements in validation set when FULL_TRAIN = True
+DECAY = False
+SHUFFLE = False
+SVM_LOSS = False
 
 
 def unpickle(file):
@@ -38,9 +43,29 @@ def compute_loss(data, labels, weight, bias):
     return (1 / data.shape[1]) * l_cross_sum
 
 
+def compute_loss_svm(data, labels, weight, bias):
+    weight_aux = np.hstack((weight, bias))  # Stack bias together with weight
+    data_aux = np.vstack((data, np.ones(data.shape[1])))  # Add row of ones for bias
+    score = weight_aux @ data_aux  # Compute the score
+    labels_aux = np.argmax(labels, axis=0)  # Get the correct class from the one hot matrix
+    target_scores = score[labels_aux, np.arange(score.shape[1])]  # Get the score of the correct class of each sample
+    margins = np.maximum(0, score - target_scores + 1)  # Compute the SVM margins
+    margins[labels_aux, np.arange(data.shape[1])] = 0  # Make zero the own margins
+    loss = np.mean(np.sum(margins, axis=0))  # Compute loss as mean of the sum per class
+
+    return loss, margins
+
+
 def compute_cost(data, labels, weight, bias, lmb):
     loss = compute_loss(data, labels, weight, bias)
-    reg = lmb * np.sum(np.sum(np.square(weight)))  # Regularization term L2
+    reg = lmb * np.sum(np.square(weight))  # Regularization term L2
+
+    return loss + reg
+
+
+def compute_cost_svm(data, labels, weight, bias, lmb):
+    loss = compute_loss_svm(data, labels, weight, bias)[0]
+    reg = lmb * np.sum(np.square(weight))  # Apply same L2 regularization used in compute_cost
 
     return loss + reg
 
@@ -59,6 +84,19 @@ def compute_grads_analytic(data, labels, weight, lmb, p):
     grad_bias = np.sum(grad_bias, axis=1)[:, np.newaxis]
 
     return grad_weight, grad_bias / data.shape[1]
+
+
+def compute_grads_analytic_svm(data, labels, weight, bias, lmb):
+    margins = compute_loss_svm(data, labels, weight, bias)[1]  # Dim: k x d
+    weight_aux = np.hstack((weight, bias))  # Stack bias together with weight
+    data_aux = np.vstack((data, np.ones(data.shape[1])))  # Row of ones for the bias term
+    labels_aux = np.argmax(labels, axis=0)  # Get the correct class from the one hot matrix
+    margins[margins > 0] = 1
+    col_sum = np.sum(margins, axis=0)  # Dim: 1 x d
+    margins[labels_aux, np.arange(data_aux.shape[1])] = -col_sum
+    grad_weight = margins @ data_aux.T
+
+    return grad_weight / data.shape[1] + 2 * lmb * weight_aux
 
 
 def preprocess_images(data, mean, std):
@@ -137,15 +175,33 @@ def plot_results(train, val, mode):
 
 
 def main():
-    np.random.seed(42)
-    # Data reading
-    file = unpickle(DATAPATH + D_BATCH[0])
-    data_train = file['data']  # Images data for training
-    labels_train = file['labels']  # Images labels for training
-    file = unpickle(DATAPATH + D_BATCH[1])
-    data_val = file['data']  # Images data for validation
-    labels_val = file['labels']  # Images labels for validation
-    file = unpickle(DATAPATH + D_BATCH[2])
+    # np.random.seed(42)
+    if FULL_TRAIN:
+        file = unpickle(DATAPATH + D_BATCH[0])
+        data_train = file['data']  # Images data for training
+        labels_train = file['labels']  # Images labels for training
+        for i in range(1, len(D_BATCH)):  # Compose the training set
+            file = unpickle(DATAPATH + D_BATCH[i])
+            data_train = np.vstack((data_train, file['data']))  # Vertically stack data
+            labels_train = np.hstack((labels_train, file['labels']))  # Horizontally stack labels
+        # Compose validation data
+        indices_val = np.random.choice(range(data_train.shape[0]), SIZE_VAL, replace=False)  # Select size_val
+        # random without repetition
+        data_val = data_train[indices_val]  # Copy selected data to validation
+        labels_val = labels_train[indices_val]  # Copy selected labels to validation
+        data_train = np.delete(data_train, indices_val, axis=0)  # Remove selected data
+        labels_train = np.delete(labels_train, indices_val)  # Remove selected labels
+    else:
+        # Reading training and validation data
+        file = unpickle(DATAPATH + D_BATCH[0])
+        data_train = file['data']  # Images data for training
+        labels_train = file['labels']  # Images labels for training
+        file = unpickle(DATAPATH + D_BATCH[1])
+        data_val = file['data']  # Images data for validation
+        labels_val = file['labels']  # Images labels for validation
+
+    # Reading test data
+    file = unpickle(DATAPATH + T_BATCH)
     data_test = file['data']  # Images data for testing
     labels_test = file['labels']  # Images labels for testing
     file = unpickle(DATAPATH + 'batches.meta')
@@ -170,8 +226,8 @@ def main():
     bias = np.random.normal(0, 0.01, (len(label_names), 1))  # Dim: k x 1
     n_batch = 100  # Define minibatch size
     n_epoch = 40  # Define number of epochs
-    lmb = 0  # Define lambda
-    eta = 0.1  # Define learning rate
+    lmb = 1  # Define lambda
+    eta = 0.001  # Define learning rate
     training_loss = list()  # Training data loss per epoch
     validation_loss = list()  # Validation data loss per epoch
     training_cost = list()  # Training data cost per epoch
@@ -180,17 +236,37 @@ def main():
     # Perform training
     print("Training model...")
     for _ in tqdm(range(n_epoch)):
+        if SHUFFLE:  # Shuffle training data and labels per epoch
+            columns = np.arange(data_train.shape[1])  # Get an array with the indeces of the columns to shuffle
+            np.random.shuffle(columns)  # Shuffle indeces
+            data_train = data_train[:, columns]  # Shuffle data columns according to the new ordered indeces
+            labels_train = labels_train[:, columns]  # Shuffle labels columns according to the new ordered indeces
         for j in range(int(data_train.shape[1] / n_batch)):
             start = j * n_batch
             end = (j + 1) * n_batch
-            delta_w, delta_b = compute_grads_analytic(data_train[:, start:end], labels_train[:, start:end], weight, lmb,
-                                                      evaluate_classifier(data_train[:, start:end], weight, bias))
+            if SVM_LOSS:
+                delta_w = compute_grads_analytic_svm(data_train[:, start:end], labels_train[:, start:end], weight,
+                                                     bias, lmb)
+                delta_b = (delta_w[:, -1])[:, np.newaxis]
+                delta_w = np.delete(delta_w, -1, axis=1)
+            else:
+                delta_w, delta_b = compute_grads_analytic(data_train[:, start:end], labels_train[:, start:end],
+                                                          weight, lmb, evaluate_classifier(data_train[:, start:end],
+                                                                                           weight, bias))
             weight -= eta * delta_w
             bias -= eta * delta_b
-        training_loss.append(compute_loss(data_train, labels_train, weight, bias))
-        validation_loss.append(compute_loss(data_val, labels_val, weight, bias))
-        training_cost.append(compute_cost(data_train, labels_train, weight, bias, lmb))
-        validation_cost.append(compute_cost(data_val, labels_val, weight, bias, lmb))
+        if DECAY:
+            eta *= 0.9
+        if SVM_LOSS:
+            training_loss.append(compute_loss_svm(data_train, labels_train, weight, bias)[0])
+            validation_loss.append(compute_loss_svm(data_val, labels_val, weight, bias)[0])
+            training_cost.append(compute_cost_svm(data_train, labels_train, weight, bias, lmb))
+            validation_cost.append(compute_cost_svm(data_val, labels_val, weight, bias, lmb))
+        else:
+            training_loss.append(compute_loss(data_train, labels_train, weight, bias))
+            validation_loss.append(compute_loss(data_val, labels_val, weight, bias))
+            training_cost.append(compute_cost(data_train, labels_train, weight, bias, lmb))
+            validation_cost.append(compute_cost(data_val, labels_val, weight, bias, lmb))
 
     visualize_weight(weight, label_names)
 
