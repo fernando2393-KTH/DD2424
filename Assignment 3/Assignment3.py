Gradient computation fixed. Mandatory part finished.

Assignment 3/Assignment3.py
@@ -53,7 +53,7 @@ def read_data(size_val=5000):
     return data_train, labels_train, data_val, labels_val, data_test, labels_test, label_names
 
 
-def initialize_network(data_train, label_names, layer_nodes, layers=2, he=False):
+def initialize_network(data_train, layer_nodes, layers=2, he=False, sigma=None):
     weights = list()
     bias = list()
     gamma = list()
@@ -64,18 +64,20 @@ def initialize_network(data_train, label_names, layer_nodes, layers=2, he=False)
         num = 1
     # Weights and bias generation:
     # 1st layer
-    weights.append(np.random.normal(0, np.sqrt(num / data_train.shape[0]),
-                                    (layer_nodes[0], data_train.shape[0])))  # Dim: m x d
-    bias.append(np.zeros((layer_nodes[0], 1)))  # Dim: m x 1
-    for i in range(1, layers - 1):  # Remaining layers except the last one
-        weights.append(np.random.normal(0, np.sqrt(num / weights[-1].shape[0]),
-                                        (layer_nodes[i], weights[-1].shape[0])))  # Dim: l x m
-        bias.append(np.zeros((layer_nodes[i], 1)))  # Dim: l x 1
-    # Last layer
-    weights.append(np.random.normal(0, np.sqrt(num / weights[-1].shape[0]),
-                                    (len(label_names), weights[-1].shape[0])))  # Dim: k x m
-    bias.append(np.zeros((len(label_names), 1)))  # Dim: k x 1
+    if sigma is not None:
+        weights.append(np.random.normal(0, sigma, (layer_nodes[0], data_train.shape[0])))  # Dim: m x d
+    else:
+        weights.append(np.random.normal(0, np.sqrt(num / data_train.shape[0]),
+                                        (layer_nodes[0], data_train.shape[0])))  # Dim: m x d
 
+    bias.append(np.zeros((layer_nodes[0], 1)))  # Dim: m x 1
+    for i in range(1, layers):  # Remaining layers
+        if sigma is not None:
+            weights.append(np.random.normal(0, sigma, (layer_nodes[i], weights[-1].shape[0])))  # Dim: l x m
+        else:
+            weights.append(np.random.normal(0, np.sqrt(num / weights[-1].shape[0]),
+                                            (layer_nodes[i], weights[-1].shape[0])))  # Dim: l x m
+        bias.append(np.zeros((layer_nodes[i], 1)))  # Dim: l x 1
     # Generate gamma and beta: n x 1 vectors
     for l_nodes in layer_nodes[:-1]:  # Generate for all hidden layers
         gamma.append(np.ones((l_nodes, 1)))  # Dim: m x 1
@@ -85,7 +87,7 @@ def initialize_network(data_train, label_names, layer_nodes, layers=2, he=False)
 
 
 def batch_normalize(s, mean, var):
-    return np.diag(pow(var + np.finfo(float).eps, -1 / 2)) @ (s.T - mean).T
+    return np.diag(pow(var + np.finfo(float).eps, -1 / 2)) @ (s - mean[:, np.newaxis])
 
 
 def batch_normalize_back_pass(g, s, mean, var):
@@ -224,10 +226,11 @@ def compute_grads_analytic(data, labels, weights, lmb, p, s_list, s_hat=None, ga
             # Update of the previous layer
             grad_weights.append((g @ data[i].T) / data[0].shape[1] + 2 * lmb * weights[i])
             grad_bias.append(np.sum(g, axis=1)[:, np.newaxis] / data[0].shape[1])
-            g = weights[i].T @ g  # Multiply by previous weight
-            diag = np.copy(data[i])  # Perform a copy of the output of the previous layer
-            diag[diag > 0] = 1  # Transform every element > 0 into 1
-            g = g * diag  # Element multiplication by diagonal of the indicator over data[i]
+            if i > 0:
+                g = weights[i].T @ g  # Multiply by previous weight
+                diag = np.copy(data[i])  # Perform a copy of the output of the previous layer
+                diag[diag > 0] = 1  # Transform every element > 0 into 1
+                g = g * diag  # Element multiplication by diagonal of the indicator over data[i]
 
         # Reverse lists to return the same order
         grad_weights.reverse(), grad_bias.reverse(), grad_gamma.reverse(), grad_beta.reverse()
@@ -394,12 +397,11 @@ def train_network(data_train, labels_train, data_val, labels_val,
 
 
 def main():
-    np.random.seed(8)
+    np.random.seed(40)
     # Read data
     data_train, labels_train, data_val, labels_val, data_test, labels_test, label_names = preprocess_data(size_val=5000)
-    # Initialize model parameters
-    weights, bias, gamma, beta = initialize_network(data_train, label_names, [50, 50, 10],
-                                                    3, he=True)
+    # Initialize model parameters for 100 features - 1000 samples
+    weights, bias, gamma, beta = initialize_network(data_train, [50, 50, 10], 3, he=True, sigma=None)
     n_batch = 100  # Define minibatch size
     eta_min = 1e-5  # Minimum value of eta
     eta_max = 1e-1  # Maximum value of eta
@@ -407,7 +409,7 @@ def main():
     n_s = 5 * int(data_train.shape[1] / n_batch)  # Step size in eta value modification
 
     # Perform training in order to get the best lambda
-    lmb_search = 8  # Number of lambda search
+    lmb_search = 20  # Number of lambda search
     n_lmb = 3  # Best n lambdas to save
     best_acc = np.zeros(lmb_search)  # The accuracies list
     best_lmb = np.zeros(lmb_search)  # The lambdas list
@@ -454,17 +456,11 @@ def main():
     print("Improved accuracies in validation: " + str(improved_acc))
     print("Improved lambdas in validation: " + str(improved_lmb))
 
-    # Training with the best found parameters
-    # indices_val = np.random.choice(range(data_val.shape[1]), 4000, replace=False)  # Select random validation samples
-    # data_train = np.hstack((data_train, data_val[:, indices_val]))  # Add previous samples to the training set
-    # labels_train = np.hstack((labels_train, labels_val[:, indices_val]))  # Add correspondent labels
-    # data_val = np.delete(data_val, indices_val, axis=1)  # Delete selected samples from validation
-    # labels_val = np.delete(labels_val, indices_val, axis=1)  # Delete correspondent labels
-
+    # Final training
     weights, bias, gamma, beta, mean, var = train_network(data_train, labels_train, data_val, labels_val,
                                                           weights, bias, gamma, beta, n_batch, eta, n_s,
-                                                          eta_min, eta_max, cycles=2, plotting=True,
-                                                          best_lambda=np.log10(0.005), lmb_search=False,
+                                                          eta_min, eta_max, cycles=3, plotting=True,
+                                                          best_lambda=improved_lmb[0], lmb_search=False,
                                                           b_norm=True)[:6]
     # Check accuracy over test data
     print("Accuracy on test data: " + str(compute_accuracy(data_test, labels_test, weights, bias,

Assignment 3/ComputeGrads.py
@@ -4,24 +4,25 @@ It also includes an example of how to compute the difference with the analytical
 """
 
 """
-def compute_grads_num_slow(data, labels, weights, bias, gamma, beta, lmb, h):
+def compute_grads_num_slow(data, labels, weights, bias, gamma, beta, lmb, h, b_norm):
     grad_weights = list()
     grad_bias = list()
     grad_gamma = list()
     grad_beta = list()
 
     for j in range(len(bias)):
-        grad_bias.append(np.zeros(len(bias[j])))
-        for i in tqdm(range(len(bias[j]))):
-            b_try = list()
-            [b_try.append(np.copy(x)) for x in bias]
-            b_try[j][i] = b_try[j][i] - h
-            c1 = compute_cost(data, labels, weights, b_try, lmb, gamma, beta)
-            b_try = list()
-            [b_try.append(np.copy(x)) for x in bias]
-            b_try[j][i] = b_try[j][i] + h
-            c2 = compute_cost(data, labels, weights, b_try, lmb, gamma, beta)
-            grad_bias[j][i] = (c2 - c1) / (2 * h)
+        grad_bias.append(np.zeros(bias[j].shape))
+        for i in tqdm(range(grad_bias[-1].shape[0])):
+            for k in range(grad_bias[-1].shape[1]):
+                b_try = list()
+                [b_try.append(np.copy(x)) for x in bias]
+                b_try[j][i, k] = b_try[j][i, k] - h
+                c1 = compute_cost(data, labels, weights, b_try, lmb, gamma, beta, None, None, b_norm)
+                b_try = list()
+                [b_try.append(np.copy(x)) for x in bias]
+                b_try[j][i, k] = b_try[j][i, k] + h
+                c2 = compute_cost(data, labels, weights, b_try, lmb, gamma, beta, None, None, b_norm)
+                grad_bias[j][i, k] = (c2 - c1) / (2 * h)
 
     for j in range(len(weights)):
         grad_weights.append(np.zeros(weights[j].shape))
@@ -30,11 +31,11 @@ def compute_grads_num_slow(data, labels, weights, bias, gamma, beta, lmb, h):
                 w_try = list()
                 [w_try.append(np.copy(x)) for x in weights]
                 w_try[j][i, k] = w_try[j][i, k] - h
-                c1 = compute_cost(data, labels, w_try, bias, lmb, gamma, beta)
+                c1 = compute_cost(data, labels, w_try, bias, lmb, gamma, beta, None, None, b_norm)
                 w_try = list()
                 [w_try.append(np.copy(x)) for x in weights]
                 w_try[j][i, k] = w_try[j][i, k] + h
-                c2 = compute_cost(data, labels, w_try, bias, lmb, gamma, beta)
+                c2 = compute_cost(data, labels, w_try, bias, lmb, gamma, beta, None, None, b_norm)
                 grad_weights[j][i, k] = (c2 - c1) / (2 * h)
 
     for j in range(len(gamma)):
@@ -44,11 +45,11 @@ def compute_grads_num_slow(data, labels, weights, bias, gamma, beta, lmb, h):
                 g_try = list()
                 [g_try.append(np.copy(x)) for x in gamma]
                 g_try[j][i, k] = g_try[j][i, k] - h
-                c1 = compute_cost(data, labels, weights, bias, lmb, g_try, beta)
+                c1 = compute_cost(data, labels, weights, bias, lmb, g_try, beta, None, None, b_norm)
                 g_try = list()
                 [g_try.append(np.copy(x)) for x in gamma]
                 g_try[j][i, k] = g_try[j][i, k] + h
-                c2 = compute_cost(data, labels, weights, bias, lmb, g_try, beta)
+                c2 = compute_cost(data, labels, weights, bias, lmb, g_try, beta, None, None, b_norm)
                 grad_gamma[j][i, k] = (c2 - c1) / (2 * h)
 
     for j in range(len(beta)):
@@ -58,33 +59,34 @@ def compute_grads_num_slow(data, labels, weights, bias, gamma, beta, lmb, h):
                 bt_try = list()
                 [bt_try.append(np.copy(x)) for x in beta]
                 bt_try[j][i, k] = bt_try[j][i, k] - h
-                c1 = compute_cost(data, labels, weights, bias, lmb, gamma, bt_try)
+                c1 = compute_cost(data, labels, weights, bias, lmb, gamma, bt_try, None, None, b_norm)
                 bt_try = list()
                 [bt_try.append(np.copy(x)) for x in beta]
                 bt_try[j][i, k] = bt_try[j][i, k] + h
-                c2 = compute_cost(data, labels, weights, bias, lmb, gamma, bt_try)
+                c2 = compute_cost(data, labels, weights, bias, lmb, gamma, bt_try, None, None, b_norm)
                 grad_beta[j][i, k] = (c2 - c1) / (2 * h)
-
 
     return grad_weights, grad_bias, grad_gamma, grad_beta
 """
 
 
 """
-# Compare outcomes (averaged)
-data, s_list, s_hat, mean_list, var_list = forward_pass(data_train, weights, bias, gamma, beta, None, None,
-                                                        b_norm=True)
-delta_w, delta_b, delta_g, delta_bt = compute_grads_analytic([i[:, 0:20] for i in data],
-                                                             labels_train[:, 0:20],
-                                                             weights, 0, data[-1][:, 0:20],
-                                                             [i[:, 0:20] for i in s_list],
-                                                             [i[:, 0:20] for i in s_hat],
+# Read data
+data_train, labels_train, data_val, labels_val, data_test, labels_test, label_names = preprocess_data(size_val=5000)
+# Initialize model parameters for 100 features - 1000 samples
+weights, bias, gamma, beta = initialize_network(data_train[0:100, 0:1000], label_names, [50, 50, 10],
+                                                3, he=True)
+data, s_list, s_hat, mean_list, var_list = forward_pass(data_train[0:100, 0:1000], weights, bias, gamma, beta, None,
+                                                        None,
+                                                        b_norm=True)  # Compute forward pass for analytic
+delta_w, delta_b, delta_g, delta_bt = compute_grads_analytic(data, labels_train[:, 0:1000],
+                                                             weights, 0, data[-1],
+                                                             s_list, s_hat,
                                                              gamma, mean_list, var_list, b_norm=True)
-
-delta_w_num, delta_b_num, delta_g_num, delta_bt_num = compute_grads_num_slow(data_train[:, 0:20],
-                                                                             labels_train[:, 0:20], weights,
+delta_w_num, delta_b_num, delta_g_num, delta_bt_num = compute_grads_num_slow(data_train[0:100, 0:1000],
+                                                                             labels_train[:, 0:1000], weights,
                                                                              bias, gamma,
-                                                                             beta, 0, 1e-5)
+                                                                             beta, 0, 1e-5, b_norm=True)
 print([np.mean(abs(delta_b[i] - delta_b_num[i])) for i in range(len(delta_b))])
 print([np.mean(abs(delta_w[i] - delta_w_num[i])) for i in range(len(delta_w))])
 print([np.mean(abs(delta_g[i] - delta_g_num[i])) for i in range(len(delta_g))])